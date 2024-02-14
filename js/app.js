00dd60208641caf9e9d9613caa6b31c2dc51aaae
Victor Jesus Galindo Ojeda <129339101+biovoid19@users.noreply.github.com>
2024-02-13 21:17:32 -0600

    Actualización1

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let audios = [
   'assets/audio/theme.mp3',
 
 ];
-let currentVolume = 1;
+let currentVolume = 0.4;
 let index = 0;
 let sound = new Audio(audios[index]);
 
@@ -93,56 +93,3 @@ function controlarVolumen() {
 
 // Llama a la función reproducirCancion para que se reproduzca automáticamente la primera canción
 reproducirCancion();
-
-
-// Agregar jugadores
-document.getElementById('addPlayer').addEventListener('click', function () {
-  const playerFields = document.getElementById('playerFields');
-  const newPlayerFieldset = document.createElement('fieldset');
-  newPlayerFieldset.classList.add('col-md-12');
-
-
-  const newPlayerLabel = document.createElement('label');
-  newPlayerLabel.textContent = 'Nombre completo:';
-  newPlayerLabel.classList.add('form-label');
-  newPlayerFieldset.appendChild(newPlayerLabel);
-
-  const newPlayerInput = document.createElement('input');
-  newPlayerInput.type = 'text';
-  newPlayerInput.classList.add('form-control');
-  newPlayerInput.name = 'playerNames[]';
-  newPlayerInput.required = true;
-  newPlayerFieldset.appendChild(newPlayerInput);
-
-  const newUserLabel = document.createElement('label');
-  newUserLabel.textContent = 'Usuario:';
-  newUserLabel.classList.add('form-label');
-  newPlayerFieldset.appendChild(newUserLabel);
-
-  const newUserInput = document.createElement('input');
-  newUserInput.type = 'text';
-  newUserInput.name = 'playerUsernames[]';
-  newUserInput.classList.add('form-control');
-  newUserInput.required = true;
-  newPlayerFieldset.appendChild(newUserInput);
-
-  const newTelLabel = document.createElement('label');
-  newTelLabel.textContent = 'Teléfono:';
-  newTelLabel.classList.add('form-label');
-  newPlayerFieldset.appendChild(newTelLabel);
-
-  const newTelInput = document.createElement('input');
-  newTelInput.type = 'text';
-  newTelInput.name = 'playerTel[]';
-  newTelInput.classList.add('form-control')
-  newPlayerFieldset.appendChild(newTelInput);
-
-  playerFields.appendChild(newPlayerFieldset);
-});
-
-document.getElementById('myForm').addEventListener('submit', function (event) {
-  // Evita el envío del formulario por defecto
-  event.preventDefault();
-
-  // Aquí puedes agregar la lógica
-});
--- js/formulario.js
+++ js/formulario.js
@@ -1,60 +1,51 @@
 // Agregar jugadores
-document.getElementById('addPlayer').addEventListener('click', function() {
+document.getElementById('addPlayer').addEventListener('click', function () {
     const playerFields = document.getElementById('playerFields');
-    playerFields.classList.add('col-md-12');
-  
-    const newPlayerFieldset1 = document.createElement('div');
-    const newPlayerFieldset2 = document.createElement('div');
-    const newPlayerFieldset3 = document.createElement('div');
-    
-    newPlayerFieldset1.classList.add('col-4');
-    newPlayerFieldset2.classList.add('col-4');
-    newPlayerFieldset3.classList.add('col-4');
-  
+    const newPlayerFieldset = document.createElement('fieldset');
+    newPlayerFieldset.classList.add('col-md-12');
+
+
     const newPlayerLabel = document.createElement('label');
     newPlayerLabel.textContent = 'Nombre completo:';
     newPlayerLabel.classList.add('form-label');
-    newPlayerFieldset1.appendChild(newPlayerLabel);
-  
+    newPlayerFieldset.appendChild(newPlayerLabel);
+
     const newPlayerInput = document.createElement('input');
     newPlayerInput.type = 'text';
     newPlayerInput.classList.add('form-control');
     newPlayerInput.name = 'playerNames[]';
     newPlayerInput.required = true;
-    newPlayerFieldset1.appendChild(newPlayerInput);
-  
+    newPlayerFieldset.appendChild(newPlayerInput);
+
     const newUserLabel = document.createElement('label');
     newUserLabel.textContent = 'Usuario:';
     newUserLabel.classList.add('form-label');
-    newPlayerFieldset2.appendChild(newUserLabel);
-  
+    newPlayerFieldset.appendChild(newUserLabel);
+
     const newUserInput = document.createElement('input');
     newUserInput.type = 'text';
     newUserInput.name = 'playerUsernames[]';
     newUserInput.classList.add('form-control');
     newUserInput.required = true;
-    newPlayerFieldset2.appendChild(newUserInput);
-  
+    newPlayerFieldset.appendChild(newUserInput);
+
     const newTelLabel = document.createElement('label');
     newTelLabel.textContent = 'Teléfono:';
     newTelLabel.classList.add('form-label');
-    newPlayerFieldset3.appendChild(newTelLabel);
-  
+    newPlayerFieldset.appendChild(newTelLabel);
+
     const newTelInput = document.createElement('input');
     newTelInput.type = 'text';
     newTelInput.name = 'playerTel[]';
     newTelInput.classList.add('form-control')
-    newPlayerFieldset3.appendChild(newTelInput);
-  
-  
-    playerFields.appendChild(newPlayerFieldset1);
-    playerFields.appendChild(newPlayerFieldset2);
-    playerFields.appendChild(newPlayerFieldset3);
-  });
-  
-  document.getElementById('myForm').addEventListener('submit', function(event) {
+    newPlayerFieldset.appendChild(newTelInput);
+
+    playerFields.appendChild(newPlayerFieldset);
+});
+
+document.getElementById('myForm').addEventListener('submit', function (event) {
     // Evita el envío del formulario por defecto
     event.preventDefault();
-  
+
     // Aquí puedes agregar la lógica
-  });
+});
